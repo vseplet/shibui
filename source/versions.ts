ebdb9df062340bd35380c579cb9d41220f0d042e
Vsevolod Pletnev <vseplet@gmail.com>
2024-06-02 03:43:05 +0700

    test tag updae

--- source/versions.ts
+++ source/versions.ts
@@ -1,6 +1,1 @@
-export default [
-  "v0.3.0.4-alpha",
-  "v0.3.0.3-alpha",
-  "v0.3.0.2-alpha",
-  "v0.3.0.1-alpha",
-];
+export default [];
--- update-version.workflow.ts
+++ update-version.workflow.ts
@@ -4,7 +4,12 @@ import { CoreStartPot } from "https://deno.land/x/shibui@v0.3.0.4-alpha/core/pot
 import { ContextPot } from "https://deno.land/x/shibui@v0.3.0.4-alpha/core/pots/ContextPot.ts";
 import { SourceType } from "https://deno.land/x/shibui@v0.3.0.4-alpha/core/types.ts";
 
+const filePath = "./source/versions.ts";
+
 class UpdateVersionContext extends ContextPot<{}> {
+  data = {
+    version: "v1",
+  };
 }
 
 const workflow = core.workflow(UpdateVersionContext)
@@ -16,13 +21,48 @@ const workflow = core.workflow(UpdateVersionContext)
       .do(async ({ pots, log, next }) => {
         const [ctx] = pots;
 
-        return next(t2);
+        const data = await Deno.readTextFile(filePath);
+        const regex = /export default \[\s*([\s\S]*?)\s*\];/;
+        const match = data.match(regex);
+
+        if (match && match[1]) {
+          const versions = match[1]
+            .split(",")
+            .map((version) => version.trim().replace(/"/g, ""))
+            .filter((version) => version !== "");
+
+          const latestVersion = versions[0];
+          const currentVersionNumber = parseInt(
+            latestVersion.substring(1),
+            10,
+          );
+          ctx.data.version = `v${currentVersionNumber + 1}`;
+
+          const newContent = `export default [ ${
+            [ctx.data.version, ...versions].map((version) => `"${version}"`)
+              .join(
+                ", ",
+              )
+          } ];`;
+
+          await Deno.writeTextFile(filePath, newContent);
+        } else {
+          const newContent = `export default ["${ctx.data.version}"];`;
+          await Deno.writeTextFile(filePath, newContent);
+        }
+
+        log.inf(ctx.data.version);
+        return next(t2, {
+          version: ctx.data.version,
+        });
       });
 
     const t2 = task1()
       .name("Update README.md")
       .do(async ({ pots, log, next }) => {
         const [ctx] = pots;
+
+        log.inf(ctx.data.version);
 
         return next(t3);
       });
@@ -31,13 +71,9 @@ const workflow = core.workflow(UpdateVersionContext)
       .name("Create TAG-NAME.txt")
       .do(async ({ pots, log, finish }) => {
         const [ctx] = pots;
-
-        const tagName = `v0.0.0.${Math.random() * 10000000 | 0}-test`;
-        await Deno.writeTextFile("TAG-NAME.txt", tagName);
-        log.inf(`generated tag in TAG-NAME.txt: ${tagName}`);
+        await Deno.writeTextFile("TAG-NAME.txt", ctx.data.version);
+        log.inf(`generated tag in TAG-NAME.txt: ${ctx.data.version}`);
         Deno.exit(0);
-
-        // return finish();
       });
 
     return t1;
