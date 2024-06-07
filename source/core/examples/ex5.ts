1f036fd72a087170aef3e8a7e04cc4f19febb269
Vsevolod Pletnev <vseplet@gmail.com>
2024-06-07 15:51:47 +0700

    Update ex5.ts

--- source/core/examples/ex5.ts
+++ source/core/examples/ex5.ts
@@ -4,7 +4,6 @@ import { CoreStartPot } from "../pots/CoreStartPot.ts";
 import core from "../mod.ts";
 import { sh } from "https://deno.land/x/shelly@v0.1.1/mod.ts";
 import type { TaskBuilder } from "../entities/TaskBuilder.ts";
-import type { IPot } from "../types.ts";
 
 export const checkUpdateTypeByCommitMessage = <
   CTX extends ContextPot<{
