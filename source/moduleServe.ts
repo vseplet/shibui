42f17575b409997d4017255d1df8c19b67e41dcd
Vsevolod Pletnev <vseplet@gmail.com>
2024-06-10 16:28:31 +0700

    update exports

--- source/moduleServe.ts
+++ source/moduleServe.ts
@@ -1,12 +1,10 @@
 Deno.serve(async (req: Request) => {
   const url = new URL(req.url);
-
   console.log(url);
   const filepath = "/" +
     decodeURIComponent(url.pathname).split("/").slice(2).join(
       "/",
     );
-
   console.log(decodeURIComponent(url.pathname));
   console.log(filepath);
   let file;
